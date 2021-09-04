ae9a60ce301b3bef969d7d9b3da864a97670b7cb
fluke006 <johongirovdiyorbek08@gmail.com>
2021-09-04 14:04:54 +0500

    add all

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const elHamburgerBtn = document.querySelector('.hamburger-btn');
-const elHeader = document.querySelector('.header-bottom');
-
-
-elHamburgerBtn.addEventListener('click', function (){
-    elHeader.classList.toggle('header-adaptive')
-})
